8ed807f7e39ad399c5a2e4f07e77f93e684cf965
junkor-1011 <test@email.com>
2020-06-11 00:48:16 +0900

    fastapi swagger

--- src/intro/intro.py
+++ src/intro/intro.py
@@ -8,3 +8,11 @@ app = FastAPI()
 async def hello():
     return {"text": "hello world!"}
 
+@app.get('/get/{path}')
+async def path_and_query_params(
+        path: str,
+        query: int,
+        # default_none: Optional[str] = None):
+        default_none: str = None):
+    return {"text": f"hello, {path}, {query} and {default_none}"}
+
