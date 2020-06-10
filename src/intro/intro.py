098d8eccb4d3ffb3c376077108fa45b6e57bb7c7
junkor-1011 <test@email.com>
2020-06-11 01:07:34 +0900

    fastapi POST

--- src/intro/intro.py
+++ src/intro/intro.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel
+from typing import Optional, List
 
 
 app = FastAPI()
@@ -25,4 +27,16 @@ async def validation(
     path: int = Path(10),
 ):
     return {"string": string, "integer": integer, "alias_query": alias_query, "path": path}
+
+
+class Data(BaseModel):
+    """Hint Class"""
+    string: str
+    default_none: Optional[int] = None
+    lists: List[int]
+
+
+@app.post('/post')
+async def declare_request_body(data: Data):
+    return {"text": f"hello, {data.string}, {data.default_none}, {data.lists}"}
 
