f8f5d165337b672152124c3249b96333a2d3f94d
test-user <test@email.com>
2020-06-24 23:04:00 +0900

    del comment

--- app/database.py
+++ app/database.py
@@ -50,12 +50,3 @@ def create_setting(using_db):
 
 
 SQLALCHEMY_DATABASE_URL, engine, SessionLocal, Base = create_setting(using_db)
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./db/app_sqlite.db"
-
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
