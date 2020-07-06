8a9b3dfd17ddfb3acd9621116cb8791d02bcca8d
test-user <test@email.com>
2020-07-07 02:59:42 +0900

    WIP: save uploaded file

--- app/routers/test_api.py
+++ app/routers/test_api.py
@@ -2,6 +2,9 @@
 test api
 """
 
+import shutil
+# import pathlib
+from pathlib import Path
 from typing import (
     Optional,
     List,
@@ -55,16 +58,37 @@ async def validation(
     }
 
 
-@router.post("/files/")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
+# @router.post("/files/")
+# async def create_file(file: bytes = File(...)):
+#     return {"file_size": len(file)}
 
 
 @router.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    return {
+        "filename": file.filename,
+        # "file_size": len(file),   # NG
+    }
+
+
+@router.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+    return {"file_sizes": [len(file) for file in files]}
 
 
 @router.post("/uploadfiles/")
 async def create_upload_files(files: List[UploadFile] = File(...)):
     return {"filenames": [file.filename for file in files]}
+
+
+# @router.post("/savefiles")
+# async def save_file(
+#     upload_file: UploadFile = File(...),
+#     destination: Path = "",
+# ):
+#     with destination.open("wb") as buffer:
+#         shutil.copyfileobj(upload_file.file, buffer)
+#     return {
+#         "filenames": upload_file.filename,
+#         "destination": str(destination),
+#     }
